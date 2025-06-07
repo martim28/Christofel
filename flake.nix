6a5cab5cfa60582dd9b02b430622c8d26723a04c
Rutherther <rutherther@ditigal.xyz>
2025-06-07 15:22:58 +0200

    chore(flake): use .net runtime 8 and 9
    The csharp-ls package requires .net runtime 9, while the application
    is still at .net 8

--- flake.nix
+++ flake.nix
@@ -80,8 +80,10 @@
           name = "christofel-dev";
           packages = [
             # Dotnet deps
-            pkgs.dotnet-sdk_8
-            pkgs.dotnet-runtime_8
+            (with pkgs.dotnetCorePackages; combinePackages [
+              sdk_8_0
+              sdk_9_0
+            ])
 
             # Services
             pkgs.mysql80
