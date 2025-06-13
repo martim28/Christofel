debe8b87b6efb94ba541b62a1645f3c397be4f56
Rutherther <rutherther@ditigal.xyz>
2025-06-13 14:17:34 +0200

    feat(ctuauth): Add functions for obtaining active and graduated studies

--- src/Libs/Christofel.CtuAuth/Extensions/KosStudentsApiExtensions.cs
+++ src/Libs/Christofel.CtuAuth/Extensions/KosStudentsApiExtensions.cs
@@ -50,6 +50,82 @@ public static class KosStudentsApiExtensions
     )
         => api.GetExtremeStudentRole(studentRoles, false, groupBy, ct);
 
+    /// <summary>
+    /// Obtains Student roles from the given list that are still
+    /// active (not terminated). The StudyState is checked for Closed,
+    /// and the EndDate is checked. The EndDate has to be null or in the future,
+    /// the studystate cannot be closed.
+    /// </summary>
+    /// <param name="api">The kos api.</param>
+    /// <param name="studentRoles">The student roles to look through.</param>
+    /// <param name="ct">The cancellation token for the operation.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    public static async Task<IReadOnlyList<Student>> GetActiveStudentRoles
+        (
+            this IKosAtomApi api,
+            List<AtomLoadableEntity<Student>>? studentRoles,
+            CancellationToken ct = default
+        )
+        => await api.FilterStudentRoles
+        (
+            studentRoles,
+            (student) => student.StudyState != StudyState.Closed && (student.EndDate is null || student.EndDate > DateTime.Now),
+            ct
+        );
+
+    /// <summary>
+    /// Obtains Student roles from the given list that are are past graduation.
+    /// Meaning the StudyTerminationReason is Graduation.
+    /// </summary>
+    /// <param name="api">The kos api.</param>
+    /// <param name="studentRoles">The student roles to look through.</param>
+    /// <param name="ct">The cancellation token for the operation.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    public static async Task<IReadOnlyList<Student>> GetGraduatedStudentRoles
+        (
+            this IKosAtomApi api,
+            List<AtomLoadableEntity<Student>>? studentRoles,
+            CancellationToken ct = default
+        )
+        => await api.FilterStudentRoles
+        (
+            studentRoles,
+            (student) => student.StudyTerminationReason == StudyTermination.Graduation,
+            ct
+        );
+
+    private static async Task<IReadOnlyList<Student>> FilterStudentRoles
+        (
+            this IKosAtomApi api,
+            List<AtomLoadableEntity<Student>>? studentRoles,
+            Func<Student, bool> filter,
+            CancellationToken ct = default
+        )
+    {
+        if (studentRoles is null)
+        {
+            return Array.Empty<Student>();
+        }
+
+        var activeStudents = new List<Student>();
+        foreach (var studentRole in studentRoles)
+        {
+            var currentStudent = await api.LoadEntityContentAsync(studentRole, token: ct);
+
+            if (currentStudent is null)
+            {
+                continue;
+            }
+
+            if (filter(currentStudent))
+            {
+                activeStudents.Add(currentStudent);
+            }
+        }
+
+        return activeStudents.AsReadOnly();
+    }
+
     private static async Task<Student?> GetExtremeStudentRole
     (
         this IKosAtomApi api,
