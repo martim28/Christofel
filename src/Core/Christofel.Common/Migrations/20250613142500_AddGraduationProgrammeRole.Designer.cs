177b6d3523a2c130d3f09554be79c6d4cbecef38
Rutherther <rutherther@ditigal.xyz>
2025-06-13 17:33:59 +0200

    chore(common): Add migration for graduation programme role
    Sets the default value of GraduationAssignemntId to current AssignmentIt.

--- src/Core/Christofel.Common/Migrations/20250613142500_AddGraduationProgrammeRole.Designer.cs
+++ src/Core/Christofel.Common/Migrations/20250613142500_AddGraduationProgrammeRole.Designer.cs
@@ -4,6 +4,7 @@ using Christofel.Common.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Christofel.Common.Migrations
 {
     [DbContext(typeof(ChristofelBaseContext))]
-    partial class ChristofelBaseContextModelSnapshot : ModelSnapshot
+    [Migration("20250613142500_AddGraduationProgrammeRole")]
+    partial class AddGraduationProgrammeRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Core/Christofel.Common/Migrations/20250613142500_AddGraduationProgrammeRole.cs
@@ -0,0 +1,66 @@
+﻿//
+//  20250613142500_AddGraduationProgrammeRole.cs
+//
+//  Copyright (c) Christofel authors. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Christofel.Common.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGraduationProgrammeRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GraduationAssignmentId",
+                schema: "Core",
+                table: "ProgrammeRoleAssignment",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProgrammeRoleAssignment_GraduationAssignmentId",
+                schema: "Core",
+                table: "ProgrammeRoleAssignment",
+                column: "GraduationAssignmentId");
+
+            // Default to the AssignmentId
+            migrationBuilder.Sql(
+                @"UPDATE `Core_ProgrammeRoleAssignment` SET `GraduationAssignmentId` = `AssignmentId`"
+            );
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProgrammeRoleAssignment_RoleAssignment_GraduationAssignmentId",
+                schema: "Core",
+                table: "ProgrammeRoleAssignment",
+                column: "GraduationAssignmentId",
+                principalSchema: "Core",
+                principalTable: "RoleAssignment",
+                principalColumn: "RoleAssignmentId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProgrammeRoleAssignment_RoleAssignment_GraduationAssignmentId",
+                schema: "Core",
+                table: "ProgrammeRoleAssignment");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProgrammeRoleAssignment_GraduationAssignmentId",
+                schema: "Core",
+                table: "ProgrammeRoleAssignment");
+
+            migrationBuilder.DropColumn(
+                name: "GraduationAssignmentId",
+                schema: "Core",
+                table: "ProgrammeRoleAssignment");
+        }
+    }
+}
